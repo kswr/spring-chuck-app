44000d6dc4daf8ad77580ed07a534c4bc4ea6282
kswr <ksawery.oleszynski@gmail.com>
2019-02-13 14:04:22 +0100

    Initial commit

---
+++ src/main/java/com/kswr/spring/chuck/springchuckapp/SpringChuckAppApplication.java
@@ -0,0 +1,14 @@
+package com.kswr.spring.chuck.springchuckapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChuckAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringChuckAppApplication.class, args);
+    }
+
+}
+
